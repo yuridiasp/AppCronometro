78259087c045218f58c2cedc65771223695c65ad
Yuri Dias Pereira <yuristardias@hotmail.com>
2022-12-30 13:20:11 -0300

    Update
    Finalizado app

--- App.js
+++ App.js
@@ -1,117 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import moment from "moment/moment";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default class AppCronometro extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {timer: '00:00:00', botao: 'Iniciar Timer'}
+    this.interval = null
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    this.iniciar = this.iniciar.bind(this)
+    this.reiniciar = this.reiniciar.bind(this)
+  }
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  reiniciar() {
+    this.parar()
+    let s = this.state
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    s.botao = 'Iniciar Timer'
+    s.timer = '00:00:00'
+    this.setState(s)
+  }
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  iniciar() {
+    let s = this.state
+    if (this.interval != null) {
+      s.botao = 'Iniciar Timer'
+      this.setState(s)
+      this.parar()
+    } else {
+      s.botao = 'Parar Timer'
+      this.setState(s)
+      this.interval = setInterval(() => {
+        let seconds = moment.duration(s.timer).asSeconds() + 1
+        s.timer = moment().startOf('day').seconds(seconds).format("HH:mm:ss")
+        this.setState(s)
+      }, 1000);
+    }
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  parar() {
+    clearInterval(this.interval)
+    this.interval = null
+  }
+
+  atualizar(texto) {
+    let s = this.state
+    s.texto = texto
+
+    this.setState(s)
+  }
+
+  render() {
+    return (
+      <View style={styles.view} >
+        <Text style={styles.titulo} >App Cronômetro</Text>
+        <View style={styles.area} >
+          <Image source={require('./images/relogio.png')} />
+          <Text style={styles.timer} >{this.state.timer}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        <View style={{flex: 5}}>
+        <TouchableOpacity style={styles.botao} onPress={this.iniciar}>
+            <View>
+              <Text style={styles.texto} >{this.state.botao}</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botao} onPress={this.reiniciar}>
+            <View>
+              <Text style={styles.texto} >Reiniciar Timer</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  titulo: {
+    flex: 1,
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 50,
+    textAlign: "center"
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  area: {
+    flex: 10,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  highlight: {
-    fontWeight: '700',
+  timer: {
+    fontSize: 25,
+    marginTop: -120,
+    color: '#BAA07A'
   },
-});
-
-export default App;
+  botao: {
+    color: '#BAA07A',
+    borderColor: '#BAA07A',
+    paddingHorizontal: 50,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 10
+  },
+  texto: {
+    fontSize: 15,
+    color: '#BAA07A',
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+})
